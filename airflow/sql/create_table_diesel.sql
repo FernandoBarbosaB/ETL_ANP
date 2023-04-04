910e7a8c1e797b735c26865f6a17bb5592845798
Fernando Barbosa <116772002+FernandoBarbosaB@users.noreply.github.com>
2023-04-04 12:55:19 -0300

    Add files via upload

---
+++ airflow/sql/create_table_diesel.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [FB_Estudos].[diesel](
+	year_month DATE,
+	uf VARCHAR(50),
+	product VARCHAR(30),
+	unit VARCHAR(2),
+	volume FLOAT,
+	created_at DATETIME
+)
---
+++ airflow/sql/create_table_petroleo.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [FB_Estudos].[derivados_petroleo](
+	year_month DATE,
+	uf VARCHAR(50),
+	product VARCHAR(30),
+	unit VARCHAR(2),
+	volume FLOAT,
+	created_at DATETIME
+)
